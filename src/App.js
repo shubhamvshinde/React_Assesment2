573287fb31bcf7b357b88b84c8c8452e144f797d
Shubham vikas shinde <78862419+shubhamvshinde@users.noreply.github.com>
2022-05-19 21:18:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Feedback from './Feedback';
+
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Feedback/>
+    </React.Fragment>
+  );
+}
+
+export default App;
+
